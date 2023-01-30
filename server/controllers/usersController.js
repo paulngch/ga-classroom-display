1b8a44562d8077110d4543a72475ff0e59e892de
paulngch <30496630+paulngch@users.noreply.github.com>
2023-01-30 22:41:59 +0800

    Init in root directory, preparing for deployment

---
+++ server/controllers/usersController.js
@@ -0,0 +1,86 @@
+const express = require("express");
+// const session = require("express-session");
+const router = express.Router();
+const Booking = require("../models/booking");
+const seed = require("../seed/seedBooking");
+
+// session
+
+// router.use(
+//   session({
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//     // cookie: { secure: true },
+//   })
+// );
+
+router.get("/seed", seed); // DELETE!
+
+router.get("/", async (req, res) => {
+  //? return [ list of bookings]
+  try {
+    const bookings = await Booking.find().exec();
+    res.json(bookings);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+router.post("/", async (req, res) => {
+  // Check for the presence of session data
+  if (!req.session.username) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+  try {
+    const booking = await Booking.create(req.body);
+    res.status(201).json(booking);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  // Check for the presence of session data
+  if (!req.session.username) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+  const { id } = req.params;
+  try {
+    const deletedBooking = await Booking.findByIdAndRemove(id);
+    res.status(200).send(deletedBooking);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const booking = await Booking.findById(id);
+    res.json(booking);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  // Check for the presence of session data
+  if (!req.session.username) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+  const { id } = req.params;
+  try {
+    const updatedBooking = await Booking.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).send(updatedBooking);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -14,4 +14,31 @@ const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
 app.use(morgan("dev"));
 
-const mongoURI = process.env.SECRET_KEY
+// Connect to Mongo
+const mongoURI = process.env.SECRET_KEY;
+const db = mongoose.connection;
+mongoose.set("runValidators", true); // here is your global setting
+mongoose.set("strictQuery", false);
+mongoose.set("debug", true);
+mongoose.connect(mongoURI);
+
+// Connection Error/Success
+db.on("error", (err) => console.log(err.message + " is mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", mongoURI));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
+app.get("/api/", (req, res) => {
+  res.json({ msg: "Hello World! It's the beginning of GA Classroom Display!" });
+});
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve("..", "client", "dist", "index.html"));
+// });
+
+//Listener
+db.once("open", () => {
+  console.log("connected to mongo", mongoURI);
+  app.listen(PORT, () => {
+    console.log("listening on port", PORT);
+  });
+});

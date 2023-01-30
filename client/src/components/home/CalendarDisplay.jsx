33c059d4c512b09e6c554a1fd9f391f80859f2f0
paulngch <30496630+paulngch@users.noreply.github.com>
2023-01-31 02:21:28 +0800

    Format calendar display

--- client/src/components/home/CalendarDisplay.jsx
+++ client/src/components/home/CalendarDisplay.jsx
@@ -57,7 +57,7 @@ export default function CalendarDisplay() {
             onChange={(e) => setSelectedDate(e.target.value)}
           ></input>
         </div>
-        <div className="mt-8 flex flex-col">
+        <div className="mt-8 flex flex-col items-center">
           <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden max-w-[1200px] shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">

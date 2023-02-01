83da8eb09fe3dcc1bae5a281d31e4b6bc26b8143
paulngch <30496630+paulngch@users.noreply.github.com>
2023-02-01 11:41:51 +0800

    Added bookings form, bookings edit, bookings delete, single booking page

--- client/src/protected/SingleCourse.jsx
+++ client/src/protected/SingleCourse.jsx
@@ -350,7 +350,6 @@ export default function SingleCourse() {
                 <button
                   type="button"
                   className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  onClick={() => resetForm()}
                 >
                   <NavLink to="/courses">Cancel</NavLink>
                 </button>
